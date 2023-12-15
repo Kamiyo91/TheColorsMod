82fb6f4d53594e18de324e899fe1f5c6c44efefa
Kamiyo <kamiyo.k91@gmail.com>
2023-12-15 12:08:39 +0100

    Hotfix

--- Reno/Buff/BattleUnitBuf_AnOldFriendGreen_C21341.cs
+++ Reno/Buff/BattleUnitBuf_AnOldFriendGreen_C21341.cs
@@ -33,11 +33,11 @@ namespace TheColorsMod_C21341.Reno.Buff
 
             var target = curCard.target;
             if (target == null || Passive?.AllyUnit == null) return;
-            OnAddBuf(-99);
             var cards = Passive.AllyUnit.allyCardDetail.GetAllDeck().Where(x =>
                 x.XmlData.Spec.Ranged != CardRange.FarArea && x.XmlData.Spec.Ranged != CardRange.FarAreaEach &&
                 x.XmlData.Spec.Ranged != CardRange.Special).ToList();
             if (!cards.Any()) return;
+            OnAddBuf(-99);
             var cardInfo = RandomUtil.SelectOne(cards);
             var card = new BattlePlayingCardDataInUnitModel
             {
