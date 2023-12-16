19031eddc652578c8cd252fb9bc66e79ca392cf7
Kamiyo <kamiyo.k91@gmail.com>
2023-12-16 12:19:13 +0100

    Passive Hotfix

--- Mio/Buff/BattleUnitBuf_MysticEyesOfHeaven_C21341.cs
+++ Mio/Buff/BattleUnitBuf_MysticEyesOfHeaven_C21341.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using LOR_DiceSystem;
+using UtilLoader21341.Extensions;
 using UtilLoader21341.Util;
 
 namespace TheColorsMod_C21341.Mio.Buff
@@ -8,26 +9,22 @@ namespace TheColorsMod_C21341.Mio.Buff
     {
         private readonly Random _random = new Random();
         private bool _activated;
-        public AtkResist AtkType = AtkResist.Normal;
         protected override string keywordId => "MysticEyes_C21341";
         protected override string keywordIconId => "MysticEyes_C21341";
 
         public override void BeforeRollDice(BattleDiceBehavior behavior)
         {
-            if (_activated) return;
-            if (behavior?.card == null || !behavior.card.CheckTargetSpeedByCard(1) ||
-                behavior.Detail == BehaviourDetail.Evasion || behavior.Detail == BehaviourDetail.Guard ||
-                !behavior.card.card.IsNotMassAttackOrSpecialAttack(99)) return;
-            if (stack > 9 || (stack > 4 && _random.Next(0, 100) <= stack * 10))
-            {
-                _activated = true;
-                OnAddBuf(-99);
-                OnAddBuf(1);
-                AtkType = AtkResist.Weak;
-            }
-
-            base.BeforeRollDice(behavior);
+            if (_activated || behavior?.card?.target == null) return;
+            if (!behavior.card.CheckTargetSpeedByCard(1) ||
+                behavior.Detail == BehaviourDetail.Evasion || behavior.Detail == BehaviourDetail.Guard) return;
+            SetDebuff(behavior.card.target, behavior.Detail, AtkResist.Normal);
+            if (stack <= 9 && (stack <= 4 || _random.Next(0, 100) > stack * 10)) return;
+            _activated = true;
+            OnAddBuf(-99);
+            OnAddBuf(1);
+            SetDebuff(behavior.card.target, behavior.Detail, AtkResist.Weak);
         }
+
 
         public override void BeforeGiveDamage(BattleDiceBehavior behavior)
         {
@@ -38,6 +35,17 @@ namespace TheColorsMod_C21341.Mio.Buff
         public override void OnRoundEndTheLast()
         {
             _activated = false;
+        }
+
+        public void SetDebuff(BattleUnitModel target, BehaviourDetail detail, AtkResist atkType)
+        {
+            if (target == null) return;
+            var oldBuff = target.GetActiveBuff<BattleUnitBuf_ResistChangeOneHitOnly_DLL21341>();
+            if (oldBuff != null) target.bufListDetail.RemoveBuf(oldBuff);
+            var buff =
+                target.OnAddBuff<BattleUnitBuf_ResistChangeOneHitOnly_DLL21341>(0) as
+                    BattleUnitBuf_ResistChangeOneHitOnly_DLL21341;
+            buff?.SetResists(atkType, detail);
         }
 
         public override void OnAddBuf(int addedStack)
