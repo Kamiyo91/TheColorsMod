c61e13a2a2a49e3dfac9b06687ba300f415d3e87
Kamiyo <kamiyo.k91@gmail.com>
2023-12-19 20:35:54 +0100

    Hotfix

--- Sae/Passive/PassiveAbility_Sae_C21341.cs
+++ Sae/Passive/PassiveAbility_Sae_C21341.cs
@@ -4,6 +4,8 @@ namespace TheColorsMod_C21341.Sae.Passive
 {
     public class PassiveAbility_Sae_C21341 : PassiveAbilityBase
     {
+        private readonly LorId _specialCard = new LorId(TheColorsModParameters.PackageId, 19);
+
         public override float GetStartHp(float hp)
         {
             return hp * 0.25f;
@@ -11,9 +13,16 @@ namespace TheColorsMod_C21341.Sae.Passive
 
         public override void OnWaveStart()
         {
-            owner.personalEgoDetail.AddCard(new LorId(TheColorsModParameters.PackageId, 19));
+            owner.personalEgoDetail.AddCard(_specialCard);
             owner.personalEgoDetail.AddCard(new LorId(TheColorsModParameters.PackageId, 16));
             UnitUtil.CheckSkinProjection(owner);
+        }
+
+        public override void OnUseCard(BattlePlayingCardDataInUnitModel curCard)
+        {
+            var cardId = curCard.card.GetID();
+            if (cardId.packageId == TheColorsModParameters.PackageId && cardId.id == _specialCard.id)
+                owner.personalEgoDetail.RemoveCard(_specialCard);
         }
     }
 }
